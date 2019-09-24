517e07854bfe9123ce446411118af68e728f37e5
Nate Kurt <nate.kurt@gmail.com>
2019-09-24 10:48:50 -0400

    added random seats file

---
+++ random_seats.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+import random, argparse
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-r", "--rows", help="how many rows defaults to 2")
+parser.add_argument("-c", "--cols", help="how many cols defaults to 2")
+args = parser.parse_args()
+
+if args.rows:
+    try:
+        rows = int(args.rows)
+
+    except:
+        print("Arguments must be integer... using 2 as default rows\n")
+        rows = 2
+else:
+    rows = 2
+
+if args.cols:
+    try:
+        cols = int(args.cols)
+    except:
+        print("Arguments must be integers... using 2 as default cols\n")
+        cols = 2
+else:
+    cols = 2
+
+name_ls = ["Nate", "Ben", "Laurent", "Gabe", "Jessy", "Imran", "Dionte", "Sakib"]
+random.shuffle(name_ls)
+
+
+string = "FRONT OF CLASS"
+print(f"{string:^50}")
+longest_name_len = max(len(x) for x in name_ls)
+
+for i in range(cols):
+    for j in range(rows):
+        if(name_ls):
+            # 2 per table so pop both names from list then put in front left.
+            name1 = name_ls.pop(0)
+            name2 = name_ls.pop(0)
+            table = f"{name1:^{longest_name_len}}   {name2:^{longest_name_len}}"
+            print(table, end="           ")
+    print("\n")
+
+
+
+
+
+    
+
+
